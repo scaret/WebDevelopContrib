4c1aabafa032627b7fcb9be50e5795212c901d49
scaret <lyc945@gmail.com>
2014-01-06 11:37:56 +0800

    added contrib spreadsheet2sjtubbs

---
+++ spreadsheet2sjtubbs/config.inc.sample.php
@@ -0,0 +1,8 @@
+<?php
+
+define('SJTUBBS_USER', 'yoursunny');
+define('SJTUBBS_PASSWD', '');
+
+$posts[0]["board"]                   = 'test';
+$posts[0]["filename"]                = 'M.1388866289.A';
+$posts[0]["spreadsheet_publish_key"] = '0As8cYqq8fm-ydGpGRzNtbkV1TFJCZEhyQzhCY2JKaEE';
