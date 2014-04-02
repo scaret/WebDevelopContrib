886ad83e0696b50a0a3cc7ee6dc7311c53182ddb
scaret <lyc945@gmail.com>
2014-04-02 20:26:25 +0800

    Add project longliveuser

---
+++ longliveuser/config.sample.php
@@ -0,0 +1,6 @@
+<?php
+
+$user[0]["userid"] = "scaret";
+$user[0]["pw"]     = "";
+$user[1]["userid"] = "sysop";
+$user[1]["pw"]     = "";
---
+++ longliveuser/longliveuser.php
@@ -0,0 +1,12 @@
+<?
+
+function getUserCh($userid){
+  // Should return a curl handle(from curl_init) that represents
+  // the LOGGEDIN user.
+  // else, return a falsy value
+}
+
+function getUserChItr(){
+  // Similar to getUserCh
+  // return User 0's at the first call, User 1's at the second call etc.
+}
